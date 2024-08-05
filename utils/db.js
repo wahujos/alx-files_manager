854ee38df7083f5f2a1dee46bcfd8e25a13e1cf4
japheth-alt163 <japhethbaraka4@gmail.com>
2024-08-05 06:19:40 -0700

    db.js

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@
 
 import { MongoClient } from 'mongodb';
 
-// Use the connection string from the environment variable
+// Use the connection string from environment variable
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/files_manager';
 
 class DBClient {
@@ -10,42 +10,24 @@ class DBClient {
     this.client = new MongoClient(MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect().then(() => {
       this.db = this.client.db(); // Use the database from the URI
-      console.log('Connected to MongoDB');
     }).catch((err) => {
       console.error('Error connecting to MongoDB:', err);
     });
   }
 
-  // Check if MongoDB is alive by pinging the server
-  async isAlive() {
-    try {
-      await this.db.command({ ping: 1 });
-      return true;
-    } catch (err) {
-      console.error('MongoDB is not alive:', err);
-      return false;
-    }
+  isAlive() {
+    // Since `isConnected` is deprecated, use the alternative method
+    return this.client.topology.isConnected();
   }
 
   async nbUsers() {
-    try {
-      return await this.db.collection('users').countDocuments();
-    } catch (err) {
-      console.error('Error counting users:', err);
-      return 0;
-    }
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    try {
-      return await this.db.collection('files').countDocuments();
-    } catch (err) {
-      console.error('Error counting files:', err);
-      return 0;
-    }
+    return this.db.collection('files').countDocuments();
   }
 }
 
 // Create and export an instance of DBClient
 const dbClient = new DBClient();
-export default dbClient;
